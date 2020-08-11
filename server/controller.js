6ac88f157bc75ffaad7238edf539ec5eee01d61e
Mahoney96 <cmahoney966@gmail.com>
2020-08-11 11:18:48 -0700

    Back-end connected

--- server/controller.js
+++ server/controller.js
@@ -1,5 +1,35 @@
+const bcrypt = require('bcryptjs')
+
 module.exports = {
-    signup: (res, req, next) => {
-        
+    signup: async (req, res, next) => {
+        const {email, password} = req.body;
+        let db = req.app.get('db');
+        let foundUser = await db.check_user_exists([email]);
+        if(foundUser[0]){
+            return res.status(409).send('User already exists')
+        }
+        let salt = bcrypt.genSaltSync(15);
+        let hash = bcrypt.hashSync(password, salt);
+        let createdUser = await db.create_user([email, hash])
+        req.session.user = {
+            id: createdUser[0].id,
+            email: createdUser[0].email
+        };
+        res.status(200).send(req.session.user)
+
+        // db.check_user_exists([email]).then(foundUser => {
+        //     if (foundUser[0]){
+        //         return res.status(409).send('User already exists')
+        //     }else {
+        //         let salt = bcrypt.genSaltSync(15);
+        //         let hash = bcrypt.hashSync(password, salt);
+        //         db.create_user([email, hash]).then( createdUser => {
+        //         req.session.user = {
+        //             id: createdUser[0].id,
+        //             email: createdUser[0].email
+        //         };
+        //         res.status(200).send(req.session.user)
+        //     }})
+        // })
     }
 }
